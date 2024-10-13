aa9d7f72091b7c6184f1b9d6cf6cc17b87e509bf
Abdullah Al Sayef <157890785+blckdrgn03@users.noreply.github.com>
2024-10-13 15:07:18 +0600

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -59,7 +59,7 @@ export default function Header() {
                 <nav className="flex justify-center gap-8 items-center">
                     {links.map((link, i) => (
                         <a 
-                            className={`${activeSection === link.name ? "text-accent font-semibold border-b-accent border-b-2" : ""} hover:text-accent transition-all duration-300 capitalize`} 
+                            className={`${activeSection === link.name ? "text-accent font-semibold border-b-accent border-b-2" : "border-b-transparent border-b-2"} hover:text-accent transition-all duration-300 capitalize`} 
                             key={i} 
                             href={link.path}
                         >

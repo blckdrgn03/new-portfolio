518a53e232d7f2d36efa13fc8ecde188a4710876
BLACKDRAGON03 <abdullahalsayef17@gmail.com>
2024-12-18 22:40:22 +0600

    orbiting techs fixed

--- components/home.jsx
+++ components/home.jsx
@@ -28,7 +28,7 @@ export default function Home() {
                     <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}} className="relative xl:pb-0 h-[250px] w-[250px] md:h-[275px] md:w-[275px] md:h-[300px] md:w-[300px] xl:h-[425px] xl:w-[425px] mb-14 md:mb-20 ">
                         <Image className="object-cover rounded-full absolute" alt="Sayef" fill quality={100} src="/pic.png"/>
 
-                        <div className="w-full -z-20 h-full absolute top-1/2 left-1/2  spinning-animation box-content opacity-66 border-slate border-[0.5px] p-[2.65rem] md:p-[3.25rem] xl:p-[3.8rem] rounded-full opacity-66">
+                        <div className="w-full -z-20 h-full absolute top-1/2 left-1/2  spinning-animation box-content opacity-50 border-slate border-[0.5px] p-[2.65rem] md:p-[3.25rem] xl:p-[3.8rem] rounded-full opacity-66">
                           <div className="w-full relative h-full">
                             <Image height={35} width={35} src="/html.svg" className="absolute spinning-animation-x xl:top-0 xl:left-0 -top-3 -left-3 " />
                             <Image height={35} width={35} src="/css.svg" className="absolute spinning-animation-x  xl:top-0 xl:right-0  -top-3 -right-3 " />

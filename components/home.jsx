5b39ce01dcc2f19dc95a62c36fd6606c565a41a1
BLACKDRAGON03 <abdullahalsayef17@gmail.com>
2024-12-18 22:27:39 +0600

    orbiting techs added

--- components/home.jsx
+++ components/home.jsx
@@ -25,8 +25,27 @@ export default function Home() {
         <>
             <section id="home" className="container pt-[7rem] md:pt-[8.5rem]">
                 <div className=" flex flex-col items-center xl:flex-row-reverse justify-between mx-auto">
-                    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}} className="relative rounded-full overflow-hidden xl:pb-0 h-[250px] w-[250px] md:h-[275px] md:w-[275px] md:h-[300px] md:w-[300px] xl:h-[475px] xl:w-[475px] mb-8">
-                        <Image className="object-cover absolute" alt="Sayef" fill quality={100} src="/pic.png"/>
+                    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}} className="relative xl:pb-0 h-[250px] w-[250px] md:h-[275px] md:w-[275px] md:h-[300px] md:w-[300px] xl:h-[425px] xl:w-[425px] mb-14 md:mb-20 ">
+                        <Image className="object-cover rounded-full absolute" alt="Sayef" fill quality={100} src="/pic.png"/>
+
+                        <div className="w-full -z-20 h-full absolute top-1/2 left-1/2  spinning-animation box-content opacity-66 border-slate border-[0.5px] p-[2.65rem] md:p-[3.25rem] xl:p-[3.8rem] rounded-full opacity-66">
+                          <div className="w-full relative h-full">
+                            <Image height={35} width={35} src="/html.svg" className="absolute spinning-animation-x xl:top-0 xl:left-0 -top-3 -left-3 " />
+                            <Image height={35} width={35} src="/css.svg" className="absolute spinning-animation-x  xl:top-0 xl:right-0  -top-3 -right-3 " />
+                            <Image height={35} width={35} src="/javascript.svg" className="absolute spinning-animation-x  xl:bottom-0 xl:right-0  -bottom-3 -right-3 " />
+                            <Image height={35} width={35} src="/react.svg" className="absolute spinning-animation-x  xl:bottom-0 xl:left-0  -bottom-3 -left-3 " />
+                            
+                          </div> 
+                        </div>
+                        <div className="w-full -z-10 h-full absolute top-1/2 left-1/2  spinning-animation-y box-content p-[3.25rem] rounded-full">
+                          <div className="w-full relative h-full">
+                            <Image height={35} width={35} src="/next.svg" className="absolute spinning-animation-x  xl:top-0 xl:left-0  -top-3 -left-3 " />
+                            <Image height={35} width={35} src="/framer.svg" className="absolute spinning-animation-x  xl:top-0 xl:right-0  -top-3 -right-3 " />
+                            <Image height={35} width={35} src="/git.svg" className="absolute spinning-animation-x  xl:bottom-0 xl:right-0  -bottom-3 -right-3 " />
+                            <Image height={35} width={35} src="/figma.svg" className="absolute spinning-animation-x  xl:bottom-0 xl:left-0  -bottom-3 -left-3 " />
+                            
+                          </div> 
+                        </div>
                     </motion.div>
                     <div>
                         <TypewriterEffectSmooth words={words} />
